c01cd4c4111dd4ff385a021c42dfdf418d8aa986
rahipat <rahirpatel0307@gmail.com>
2025-11-12 20:02:18 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -15,10 +15,20 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtract(3, 5), -2)
         self.assertEqual(subtract(0, 0), 0)
 
-    def test_div_by_zero(self):
+    def test_multipy(self):
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-2, 3), -6)
+        self.assertEqual(mul(0, 5), 0)
+
+    def test_divide(self):
+        self.assertEqual(div(6, 3), 2)
+        self.assertEqual(div(-6, 3), -2)
+        self.assertAlmostEqual(div(7, 2), 3.5)
+    
+    def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             div(5, 0)
-
+    
     def test_logarithm(self):
         self.assertEqual(logarithm(8, 2), 3)
         self.assertEqual(logarithm(100, 10), 2)
@@ -26,36 +36,29 @@ class TestCalculator(unittest.TestCase):
 
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            logarithm(10, 1)
-
-    def test_mul(self):
-        self.assertEqual(mul(2, 3), 6)
-        self.assertEqual(mul(-2, 3), -6)
-        self.assertEqual(mul(0, 5), 0)
-
-    def test_div(self):
-        self.assertEqual(div(6, 3), 2)
-        self.assertEqual(div(-6, 3), -2)
-        self.assertAlmostEqual(div(7, 2), 3.5)
+            logarithm(6, 1)  
+        with self.assertRaises(ValueError):
+            logarithm(7, 0)  
+        with self.assertRaises(ValueError):
+            logarithm(67, -2)
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            logarithm(-5, 10)
-
+            logarithm(0, 6)
+        with self.assertRaises(ValueError):
+            logarithm(-6, 6)
+            
     def test_hypotenuse(self):
         self.assertEqual(hypotenuse(3, 4), 5)
         self.assertEqual(hypotenuse(5, 12), 13)
         self.assertAlmostEqual(hypotenuse(8, 15), 17)
 
-    def test_square_root(self):
+    def test_sqrt(self):
         self.assertEqual(square_root(9), 3)
+        self.assertAlmostEqual(square_root(2), 1.4142, places=4)
         with self.assertRaises(ValueError):
             square_root(-4)
 
-    def test_exp(self):
-        self.assertEqual(exp(2, 3), 8)
-        self.assertEqual(exp(5, 0), 1)
-        self.assertEqual(exp(3, 2), 9)
 
 # Do not touch this
 if __name__ == "__main__":
